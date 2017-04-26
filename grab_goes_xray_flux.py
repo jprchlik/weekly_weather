9fdcf4d916c794d377e4f572df650ec568ca6709
jprchlik <japrchlik@gmail.com>
2017-04-26 09:36:05 -0400

    Added download of ace data for solar wind

--- grab_goes_xray_flux.py
+++ grab_goes_xray_flux.py
@@ -20,6 +20,20 @@ def getfiles(day,ftp,sdir):
 
     fhandle.close()
 
+#Funtion to retrieve events from given day on noao ftp server
+def getfiles_ace(day,ftp,sdir,swepam=False,mag=False):
+    if swepam: files = '{0:%Y%m%d}_ace_swepam_1m.txt'.format(day)
+    if mag: files = '{0:%Y%m%d}_ace_mag_1m.txt'.format(day)
+#create file to write ftp data
+    fhandle = open(sdir+'/ace/'+files,'wb')
+    try:
+        ftp.retrbinary('RETR {0}'.format(files),fhandle.write)
+    except:
+        print '{0} not in archive'.format(files)
+
+    fhandle.close()
+
+
 def look_xrays(start,end,sdir):
 #initialize variables
     ftp = ftplib.FTP('ftp.swpc.noaa.gov','anonymous','jakub.prchlik@cfa.harvard.edu')
@@ -31,6 +45,11 @@ def look_xrays(start,end,sdir):
     for day in days:
 #request file from ftp server to add to local directory
         getfiles(day,ftp,sdir)
+#change ftp directory to events directory for a given year
+    ftp.cwd('../ace/')
+    for day in days:
+        getfiles_ace(day,ftp,sdir,swepam=True)
+        getfiles_ace(day,ftp,sdir,mag=True)
     
 #nicely leave the ftp server
     ftp.quit()

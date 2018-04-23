b7ccd640473610910659c7e670b3920b092748c1
jprchlik <japrchlik@gmail.com>
2018-04-23 11:28:37 -0400

    Changed to class

--- get_synoptic_files.py
+++ get_synoptic_files.py
@@ -5,78 +5,92 @@ import itertools
 from multiprocessing import Pool
 
 
-#retrieve desired cadence from file list
-def des_cad(start,end,delta):
-    """Create an array from start to end with desired cadence"""
-    curr = start
-    while curr < end:
-        yield curr
-        curr += delta
-
-#wrapper for download file for par. processing
-def wrap_download_file(args):
-    return download_file(*args)
-
-#download files from archive for each wavelength
-def download_file(time,wavl):
-   global w_fmt,f_dir,b_dir
-   #format wavelength
-   w_fil = w_fmt.format(wavl)
-   #format input time
-   s_dir = f_dir.format(time)
-   #create output file
-   o_fil = b_dir+s_dir.split('/')[-1]+w_fil
-   #file to download from archive
-   d_fil = syn_arch+s_dir+w_fil
-
-   #check if output file exists
-   if os.path.isfile(o_fil) == False:
-       #try to download file if fails continue on
-       try:
-           urllib.urlretrieve(d_fil,o_fil) 
-       except:
-           print("Cound not Download {0} from archive".format(d_fil))
 
 
 
-def main(b_dir,stime,etime,caden,syn_arch= 'http://jsoc.stanford.edu/data/aia/synoptic/',d_wav=[193],
-         w_fmt='{0:04d}.fits',nproc=8)
+class download:
 
+    def __init__(stime,etime,caden,b_dir,syn_arch= 'http://jsoc.stanford.edu/data/aia/synoptic/',d_wav=[193],
+             w_fmt='{0:04d}.fits',nproc=8)
+    
+        """
+    
+        Parameters:
+        -----------
+        stime: datetime object
+            The time to start downloading AIA files.
+      
+        etime: datetime object
+            The time to end downloading AIA files.
+        
+        caden: datetime time delta object 
+            The time cadence to download AIA files.
+        
+        b_dir: string
+            The base directory for locally storing the AIA archive.
+        
+        syn_arch: string, optional
+            Location of online syntopic archive (default = 'http://jsoc.stanford.edu/data/aia/synoptic/').
+        
+        d_wav: list, optional
+            List of wavelengths to download from the online archive (Default = [193]).
+        
+        w_fmt: list, optional
+            The wavelength format of the wavelength list (defatult = '{0:04d}.fits').
+        
+        f_dir : string, optional
+            Create local directory path format for SDO/AIA files (default = '{0:%Y/%m/%d/H%H00/AIA%Y%m%d_%H%M_}')
+    
+        """
+    
+    
+        
+        #desired cadence for the observations
+        real_cad = [result for result in des_cad(stime,etime,caden)]
+        
+        #create a list of combination of dates and wavelengths
+        inpt_itr = list(itertools.product(real_cad,d_wav))
+        
+        #Download the files locally
+        pool = Pool(processes=8)
+        outp = pool.map(wrap_download_file,inpt_itr)
+        pool.close()
+        pool.join()
 
+    #retrieve desired cadence from file list
+    def des_cad(start,end,delta):
+        """Create an array from start to end with desired cadence"""
+        curr = start
+        while curr < end:
+            yield curr
+            curr += delta
+    
+    #wrapper for download file for par. processing
+    def wrap_download_file(args):
+        return download_file(*args)
+    
+    #download files from archive for each wavelength
+    def download_file(time,wavl):
+  
+       #Init variables
+       w_fmt = self.w_fmt
+       f_dir = self.f_dir
+       b_dir = self.b_dir
 
-    #base local SDO archive directory
-    b_dir = 'sdo_archive/'
+       #format wavelength
+       w_fil = w_fmt.format(wavl)
+       #format input time
+       s_dir = f_dir.format(time)
+       #create output file
+       o_fil = b_dir+s_dir.split('/')[-1]+w_fil
+       #file to download from archive
+       d_fil = syn_arch+s_dir+w_fil
     
+       #check if output file exists
+       if os.path.isfile(o_fil) == False:
+           #try to download file if fails continue on
+           try:
+               urllib.urlretrieve(d_fil,o_fil) 
+           except:
+               print("Cound not Download {0} from archive".format(d_fil))
     
-    #location of syntopics
-    syn_arch = 'http://jsoc.stanford.edu/data/aia/synoptic/'
-    
-    #Wavelength download
-    d_wav = [94, 193, 211, 131]
-    
-    #wavelength format
-    w_fmt = '{0:04d}.fits'
-    
-    #create directory path minus the wavelength
-    f_dir = '{0:%Y/%m/%d/H%H00/AIA%Y%m%d_%H%M_}'
-    
-    #get starttime for observations
-    stime = datetime(2010,5,13,0,0,0)
-    
-    #set endtime for observations
-    etime = datetime.utcnow()
-    
-    #cadence to get observations
-    caden = timedelta(minutes=30)
-    
-    #desired cadence for the observations
-    real_cad = [result for result in des_cad(stime,etime,caden)]
-    
-    #create a list of combination of dates and wavelengths
-    inpt_itr = list(itertools.product(real_cad,d_wav))
-    
-    #Download the files locally
-    pool = Pool(processes=8)
-    outp = pool.map(wrap_download_file,inpt_itr)
-    pool.close()
-    pool.join()
